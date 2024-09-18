236ab770b07aeb96fc630bb4d427b2e02b6dfae2
GirishJain <giri.jain14@gmail.com>
2024-09-17 19:29:52 -0600

    Commented IsDevelopment check

--- Program.cs
+++ Program.cs
@@ -98,11 +98,11 @@ builder.Services.AddHttpClient<IFMPService, FMPService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
